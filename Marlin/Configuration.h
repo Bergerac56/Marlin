e799179cc04e84e38df767c3b26999c3e03c8feb
Olivier de Broqueville <bergerac@xerus.be>
2019-07-07 10:53:30 +0200

    Encore des tweeks

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -81,7 +81,7 @@
 // build by the user have been successfully uploaded into firmware.
 #define STRING_CONFIG_H_AUTHOR "O. de Broqueville" // Who made the changes.
 #define SHOW_BOOTSCREEN
-#define STRING_SPLASH_LINE1 "V2 BF179 06-07-2019" // will be shown during bootup in line 1
+#define STRING_SPLASH_LINE1 "V2 BF179 07-07-2019" // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 "www.xerus.be"      // will be shown during bootup in line 2
 
 /**
