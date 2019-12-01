d47862f0633caf930686259b017affe473cc7e24
Olivier de Broqueville <bergerac@xerus.be>
2019-12-01 12:23:00 +0100

    Mise à niveau BF 197

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -142,7 +142,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Jacbot OdB V2 BF196   "
+#define CUSTOM_MACHINE_NAME "Jacbot OdB V2 BF197   "
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
