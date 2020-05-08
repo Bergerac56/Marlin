0fbdcaa7c80523e4eab9f131d1fc2c738c339b46
Olivier de Broqueville <bergerac01@xerus.be>
2020-05-08 17:21:27 +0200

    Correction options non conformes

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1069,7 +1069,7 @@
 
   // Reverse SD sort to show "more recent" files first, according to the card's FAT.
   // Since the FAT gets out of order with usage, SDCARD_SORT_ALPHA is recommended.
-  #define SDCARD_RATHERRECENTFIRST
+  // #define SDCARD_RATHERRECENTFIRST  // Jacbot
 
   #define SD_MENU_CONFIRM_START             // Confirm the selected SD file before printing
 
@@ -1132,14 +1132,14 @@
 
   // SD Card Sorting options
   #if ENABLED(SDCARD_SORT_ALPHA)
-    #define SDSORT_LIMIT       100    // Maximum number of sorted items (10-256). Costs 27 bytes each. Origine = 40
+    #define SDSORT_LIMIT       256    // Maximum number of sorted items (10-256). Costs 27 bytes each. Origine = 40
     #define FOLDER_SORTING     -1     // -1=above  0=none  1=below
     #define SDSORT_GCODE       true   // Allow turning sorting on/off with LCD and M34 g-code.
     #define SDSORT_USES_RAM    true   // Pre-allocate a static array for faster pre-sorting.
     #define SDSORT_USES_STACK  true   // Prefer the stack for pre-sorting to give back some SRAM. (Negated by next 2 options.)
     #define SDSORT_CACHE_NAMES false  // Keep sorted items in RAM longer for speedy performance. Most expensive option.
     #define SDSORT_DYNAMIC_RAM false  // Use dynamic allocation (within SD menus). Least expensive option. Set SDSORT_LIMIT before use!
-    #define SDSORT_CACHE_VFATS 2      // Maximum number of 13-byte VFAT entries to use for sorting.
+    #define SDSORT_CACHE_VFATS 4      // Maximum number of 13-byte VFAT entries to use for sorting.
                                       // Note: Only affects SCROLL_LONG_FILENAMES with SDSORT_CACHE_NAMES but not SDSORT_DYNAMIC_RAM.
   #endif
 
@@ -3346,7 +3346,7 @@
 //
 // M43 - display pin status, toggle pins, watch pins, watch endstops & toggle LED, test servo probe
 //
-//#define PINS_DEBUGGING  // TEST pour Jacbot  A enlever
+//define PINS_DEBUGGING  // TEST pour Jacbot  A enlever
 
 // Enable Marlin dev mode which adds some special commands
 //#define MARLIN_DEV_MODE
