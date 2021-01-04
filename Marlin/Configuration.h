7db112bdecd9e53f1c90c41bd0825cccd4e42fb1
Olivier de Broqueville <bergerac01@xerus.be>
2021-01-04 11:36:19 +0100

    V2-BF238

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -139,7 +139,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Jacbot OdB V2-BF237  "
+#define CUSTOM_MACHINE_NAME "Jacbot OdB V2-BF238  "
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
